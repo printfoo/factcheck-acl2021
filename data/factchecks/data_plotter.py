ddc03f3b58f5858525028e067f2ae67c6fd3c004
Shan Jiang <shan.sam.jiang@gmail.com>
2020-04-09 19:27:19 -0400

    update factcheck data

--- data/factchecks/data_plotter.py
+++ data/factchecks/data_plotter.py
@@ -21,28 +21,37 @@ class DataPlotter(object):
 
     def plot(self):
 
-        """
         # Load factchecks (train set).
         factcheck_path = os.path.join(self.data_dir, "train.tsv")
         factcheck = pd.read_csv(factcheck_path, delimiter="\t", names=["label", "tokens"])
 
         # Stats.
-        factcheck["len"] = factcheck["tokens"].apply(lambda t: len(t.split(" ")))
+        factcheck["tokens"] = factcheck["tokens"].apply(lambda t: t.split(" "))
+        factcheck["len"] = factcheck["tokens"].apply(lambda t: len(t))
+        print(factcheck[factcheck["len"] > 2048])
         fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(5, 3))
         bins = [100 * i for i in range(30)]
         factcheck["len"].plot.hist(ax=ax, bins=bins, color="k", lw=0, alpha=0.6)
         plt.savefig(os.path.join("figs", "token_len.pdf"), bbox_inches="tight", pad_inches=0)
-        """
         
         # Load word weights from logistic regression.
         word_weight_path = os.path.join(self.data_dir, "linear.analyze", "word_weight.json")
-        with open(word_weight_path, "r") as f:
-            word_weight = json.loads(f.read())
-        for label in word_weight:
-            pos = [(k, v) for k, v in word_weight[label].items() if v > 0]
-            
-            print(label, len(pos))
+        ww = pd.read_json(word_weight_path, lines=True)
+        ww["w"] = ww["false"].abs() + ww["true"].abs() + \
+                  ww["mixture"].abs() + ww["unproven"].abs()
+        top_w = set(ww.sort_values("w").tail(100)["word"])
         
+        # Find word location.
+        factcheck["locs"] = factcheck["tokens"].apply(lambda t: [i for i, w in enumerate(t) if w in top_w])
+        locs = factcheck["locs"].apply(pd.Series).unstack().reset_index(drop=True).dropna()
+        
+        # Plot word location.
+        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(5, 3))
+        bins = [100 * i for i in range(30)]
+        locs.plot.hist(ax=ax, bins=bins, color="k", lw=0, alpha=0.6)
+        plt.savefig(os.path.join("figs", "token_loc.pdf"), bbox_inches="tight", pad_inches=0)
+        print(locs)
+
 
 if __name__ == "__main__":
     DataPlotter().plot()
