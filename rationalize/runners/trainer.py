e8da77e4ef7514dbc30871c4c74a6260b799327e
printfoo <shan.sam.jiang@gmail.com>
2020-04-23 16:15:34 -0400

    update trainer

--- rationalize/runners/trainer.py
+++ rationalize/runners/trainer.py
@@ -26,20 +26,26 @@ def train(model, data, args):
     for i in tqdm(range(args.num_iteration + 1)):
 
         model.train()  # Set model to train mode.
-        x, y, _, m = data.get_train_batch(batch_size=args.batch_size,
-                                          sort=True)  # Sample a batch.
+        samples = data.get_train_batch(args.batch_size, sort=True)  # Sample a batch.
+        x, y, m, r, s, d = samples
 
         # Save values to torch tensors.
         x = Variable(torch.from_numpy(x))
         y = Variable(torch.from_numpy(y))
         m = Variable(torch.from_numpy(m)).float()
+        r = Variable(torch.from_numpy(r)).float()
+        s = Variable(torch.from_numpy(s)).float()
+        d = Variable(torch.from_numpy(d)).float()
         if args.cuda:
             x = x.cuda()
             y = y.cuda()
             m = m.cuda()
+            r = r.cuda()
+            s = s.cuda()
+            d = d.cuda()
 
         # Train one step.
-        losses, predict, z = model.train_one_step(x, y, m)
+        losses, predict, z = model.train_one_step(x, y, m, r, s, d)
 
         # Evaluate classification accuracy.
         _, y_pred = torch.max(predict, dim=1)
@@ -53,7 +59,8 @@ def train(model, data, args):
             z_ = z.data[2,:]
             z_b = torch.zeros_like(z)
             z_b_ = z_b.data[2,:]
-            print("gold label:", data.idx2label[y_.item()], "pred label:", data.idx2label[pred_.item()])
+            print("gold label:", data.idx2label[y_.item()])
+            print("pred label:", data.idx2label[pred_.item()])
             data.display_example(x_, z_)
 
         # Eval every args.eval_iteration.
