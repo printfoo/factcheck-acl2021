c64e44eca8e42521c23e0d7f61b8d5d0317e36ba
Shan Jiang <shan.sam.jiang@gmail.com>
2020-04-29 21:30:21 -0400

    update download bash

--- data/personal_attacks/raw/download.sh
+++ data/personal_attacks/raw/download.sh
@@ -1,4 +1,5 @@
 wget "https://s3-eu-west-1.amazonaws.com/pfigshare-u-files/7554634/attack_annotated_comments.tsv"
 wget "https://s3-eu-west-1.amazonaws.com/pfigshare-u-files/7554637/attack_annotations.tsv"
-wget "https://github.com/shcarton/rcnn/raw/master/deliberativeness/data/processed/wiki/personal_attacks/wiki_attack_dev_rationale.csv"
-wget "https://github.com/shcarton/rcnn/raw/master/deliberativeness/data/processed/wiki/personal_attacks/wiki_attack_test_rationale.csv"
+wget "https://raw.githubusercontent.com/shcarton/rcnn/master/deliberativeness/data/processed/wiki/personal_attacks/wiki_attack_dev_rationale.csv"
+wget "https://raw.githubusercontent.com/shcarton/rcnn/master/deliberativeness/data/processed/wiki/personal_attacks/wiki_attack_test_rationale.csv"
+wget "https://raw.githubusercontent.com/uds-lsv/lexicon-of-abusive-words/master/Lexicons/baseLexicon.txt"
