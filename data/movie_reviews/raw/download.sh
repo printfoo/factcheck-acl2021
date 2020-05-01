cc797a71192b0fd6683e3b16bebf65fda75e62e4
Shan Jiang <shan.sam.jiang@gmail.com>
2020-05-01 19:33:29 -0400

    add emolex lexicon

--- data/movie_reviews/raw/download.sh
+++ data/movie_reviews/raw/download.sh
@@ -1,2 +1,4 @@
 wget "http://www.eraserbenchmark.com/zipped/movies.tar.gz"
 tar -xvf "movies.tar.gz"
+wget "http://sentiment.nrc.ca/lexicons-for-research/NRC-Emotion-Lexicon.zip"
+unzip "NRC-Emotion-Lexicon.zip"
